b8f617e2bbbab01ffa0823a1e23967eb91b42678
哈喽 <981349873@qq.com>
2016-08-17 16:46:48 +0800

    爱好地方hi

--- theme/test.php
+++ theme/test.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>无标题文档</title>
+<title>无fdhkjbhj标题文档</title>
 <script src="<?php echo base_url();?>style/js/jquery-1.9.1.min.js"></script>
 <script language="javascript">
 $(document).ready(function(){
